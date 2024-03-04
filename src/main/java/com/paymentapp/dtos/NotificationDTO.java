559ad493ee66464435ae079238f848a5474981fe
Luan Valente <luanbvalente@hotmail.com>
2024-03-04 19:44:24 -0300

    RestTemplate for Authorization and Notification Service

---
+++ src/main/java/com/paymentapp/dtos/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.paymentapp.dtos;
+
+public record NotificationDTO(String email, String message) {
+}
--- src/main/java/com/paymentapp/services/TransactionService.java
+++ src/main/java/com/paymentapp/services/TransactionService.java
@@ -5,9 +5,14 @@ import com.paymentapp.domain.user.User;
 import com.paymentapp.dtos.TransactionDTO;
 import com.paymentapp.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -18,14 +23,19 @@ public class TransactionService {
     @Autowired
     private TransactionRepository transactionsRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Transaction createTransaction(TransactionDTO transactionDTO) throws Exception {
         User payer = userService.findUserById(transactionDTO.payer_id());
         User payee = userService.findUserById(transactionDTO.payee_id());
 
-        System.out.println(payer);
-        System.out.println(payee);
-
         userService.validTransfer(payer, transactionDTO.amount());
+
+        boolean isAuthorized = this.authorizeTransaction(payer, transactionDTO.amount());
+        if (!isAuthorized) {
+            throw new Exception("Transação não Autorizada!");
+        }
 
         Transaction newtransaction = new Transaction();
         newtransaction.setAmount(transactionDTO.amount());
@@ -42,4 +52,18 @@ public class TransactionService {
 
         return newtransaction;
     }
+
+    public boolean authorizeTransaction(User payer, BigDecimal amount) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else {
+            return false;
+        }
+
+    }
+
+
 }
